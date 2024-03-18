e0d2ece91f0d7b08ccf6f0eb3e09623a23324f87
KSIMAN <somnia.universum@gmail.com>
2024-03-19 01:06:05 +0300

    Quest System experiments

--- Source/LoryShelter/Logics/QuestSystemComponent.h
+++ Source/LoryShelter/Logics/QuestSystemComponent.h
@@ -4,11 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "../Items/InteractItem.h"
 #include "QuestSystemComponent.generated.h"
 
 class UQuestItem;
 class UQuestPanel;
-
+class AInteractionItem;
 class ALoryShelterCharacter; //On Future
 
 
@@ -17,12 +18,15 @@ class LORYSHELTER_API UQuestSystemComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+	DECLARE_MULTICAST_DELEGATE_TwoParams(FInteractionHappened, AInteractItem*, int);
+
 public:	
 	// Sets default values for this component's properties
 	UQuestSystemComponent();
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	FInteractionHappened testDelegate;
 
 	//--Getters--------------------------
 
