73e6ad792eb03f179904d7c27faa7c726fc16a8f
KSIMAN <somnia.universum@gmail.com>
2024-03-29 02:13:50 +0300

    For prev commit

---
+++ Source/LoryShelter/UI/PlantWidgets/PlantTimerWidget.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlantTimerWidget.h"
+
+void UPlantTimerWidget::setItemName(const FText& newItemName)
+{
+	ItemNameText->SetText(newItemName);
+}
+
+void UPlantTimerWidget::setTimeRemained(int seconds)
+{
+	TimeRemainedText->SetText(secondsToLineText(seconds));
+}
+
+FText UPlantTimerWidget::secondsToLineText(int seconds)
+{
+	if (seconds == 0)
+		return FText::FromStringTable(FName("ItemsST"), TEXT("HARVEST_READY"));
+
+	int minutes = seconds / 60;
+	int hours = minutes / 60;
+
+	seconds -= minutes * 60;
+	hours -= minutes * 60;
+
+	return FText::Format(NSLOCTEXT("QuestSpace", "StepInfoText", " {0}:{1}:{2} "), hours, minutes, seconds);
+}
---
+++ Source/LoryShelter/UI/PlantWidgets/PlantTimerWidget.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
+#include "PlantTimerWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LORYSHELTER_API UPlantTimerWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	//sets Planting Item Name
+	void setItemName(const FText& newItemName);
+
+	//updates Info about remained time
+	void setTimeRemained(int seconds);
+
+protected:
+
+	//--UI Components--------------------------------------------------
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* ItemNameText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* TimeRemainedText;
+
+	//returns line with time in format 00:00:00
+	//if seconds == 0 returns "Ready to harvest" 
+	FText secondsToLineText(int seconds);
+	
+};
